6fd958184e918cdc235a69e622960f9e8c539004
hakanerdal <hakan.erdal@sanpark.com>
2020-04-29 13:55:31 +0300

    Update LARAVEL_MODEL_MIGRATION_PROPERTIES.php

--- templates/source/LARAVEL_MODEL_MIGRATION_PROPERTIES.php
+++ templates/source/LARAVEL_MODEL_MIGRATION_PROPERTIES.php
@@ -1,41 +1,35 @@
 {{@if {{$ ../property_type/__value__}} == "Checkbox"}}
-			$table->smallInteger('{{$ ../name/__value__/__lowercase_text__}}');
+			$table->smallInteger('{{$ ../name/__value__/__lowercase_text__}}')->default(0);
 {{@elseif {{$ ../property_type/__value__}} == "Date"}}
-			$table->date('{{$ ../name/__value__/__lowercase_text__}}');
+			$table->date('{{$ ../name/__value__/__lowercase_text__}}')->default(NULL);
 {{@elseif {{$ ../property_type/__value__}} == "DateTime"}}
-			$table->dateTime('{{$ ../name/__value__/__lowercase_text__}}');
-{{@elseif {{$ ../property_type/__value__}} == "Selection"}}
-			$table->smallInteger('{{$ ../name/__value__/__lowercase_text__}}');
+			$table->dateTime('{{$ ../name/__value__/__lowercase_text__}}')->default('0000-00-00 00:00:00');
+{{@elseif {{$ ../selection_properties/property_model_multiple_selection/__value__}} == "0"}}
+			$table->smallInteger('{{$ ../name/__value__/__lowercase_text__}}')->default(0);
 {{@elseif {{$ ../property_type/__value__}} == "EmailAddress"}}
-			$table->string('{{$ ../name/__value__/__lowercase_text__}}');
-{{@elseif {{$ ../property_type/__value__}} == "FileList"}}
-			$table->longText('{{$ ../name/__value__/__lowercase_text__}}');
+			$table->string('{{$ ../name/__value__/__lowercase_text__}}')->default(NULL);
 {{@elseif {{$ ../property_type/__value__}} == "HTMLEditor"}}
-			$table->longText('{{$ ../name/__value__/__lowercase_text__}}');
-{{@elseif {{$ ../property_type/__value__}} == "ImageList"}}
-			$table->longText('{{$ ../name/__value__/__lowercase_text__}}');
+			$table->longText('{{$ ../name/__value__/__lowercase_text__}}')->default(NULL);
 {{@elseif {{$ ../property_type/__value__}} == "Integer"}}
-			$table->bigInteger('{{$ ../name/__value__/__lowercase_text__}}');
+			$table->bigInteger('{{$ ../name/__value__/__lowercase_text__}}')->default(0);
 {{@elseif {{$ ../property_type/__value__}} == "Location"}}
-			$table->string('{{$ ../name/__value__/__lowercase_text__}}');
+			$table->string('{{$ ../name/__value__/__lowercase_text__}}')->default(NULL);
 {{@elseif {{$ ../property_type/__value__}} == "LongText"}}
-			$table->longText('{{$ ../name/__value__/__lowercase_text__}}');
+			$table->longText('{{$ ../name/__value__/__lowercase_text__}}')->default(NULL);
 {{@elseif {{$ ../property_type/__value__}} == "Number"}}
-			$table->double('{{$ ../name/__value__/__lowercase_text__}}', 8, 2);
-{{@elseif {{$ ../property_type/__value__}} == "ClassSlection"}}
-			$table->longText('{{$ ../name/__value__/__lowercase_text__}}');
+			$table->double('{{$ ../name/__value__/__lowercase_text__}}', 8, 2)->default(0.0);
 {{@elseif {{$ ../property_type/__value__}} == "Password"}}
-			$table->longText('{{$ ../name/__value__/__lowercase_text__}}');
+			$table->longText('{{$ ../name/__value__/__lowercase_text__}}')->default(NULL);
 {{@elseif {{$ ../property_type/__value__}} == "PhoneNumber"}}
-			$table->string('{{$ ../name/__value__/__lowercase_text__}}');
+			$table->string('{{$ ../name/__value__/__lowercase_text__}}')->default(NULL);
 {{@elseif {{$ ../property_type/__value__}} == "Radio"}}
-			$table->bigInteger('{{$ ../name/__value__/__lowercase_text__}}');
+			$table->bigInteger('{{$ ../name/__value__/__lowercase_text__}}')->default(0);
 {{@elseif {{$ ../property_type/__value__}} == "ShortText"}}
-			$table->string('{{$ ../name/__value__/__lowercase_text__}}');
+			$table->string('{{$ ../name/__value__/__lowercase_text__}}')->default(NULL);
 {{@elseif {{$ ../property_type/__value__}} == "Switch"}}
-			$table->smallInteger('{{$ ../name/__value__/__lowercase_text__}}');
+			$table->smallInteger('{{$ ../name/__value__/__lowercase_text__}}')->default(0);
 {{@elseif {{$ ../property_type/__value__}} == "Time"}}
-			$table->time('{{$ ../name/__value__/__lowercase_text__}}');
+			$table->time('{{$ ../name/__value__/__lowercase_text__}}')->default('00:00:00');
 {{@elseif {{$ ../property_type/__value__}} == "URLAddress"}}
-			$table->string('{{$ ../name/__value__/__lowercase_text__}}');
+			$table->string('{{$ ../name/__value__/__lowercase_text__}}')->default(NULL);
 {{@endif}}
