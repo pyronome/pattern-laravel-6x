7c3e7e379506c0528501a78d186f416a23ecf3db
hakan erdal <hakan.erdal@sanpark.com>
2020-10-28 18:00:11 +0300

    add snippets code to AuthServiceProvider

---
+++ files/source/app/Providers/AuthServiceProvider.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Providers;
+
+use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
+use Illuminate\Support\Facades\Gate;
+/* {{@snippet:begin_class}} */
+
+class AuthServiceProvider extends ServiceProvider
+{
+    /* {{@snippet:begin_variables}} */
+
+    /**
+     * The policy mappings for the application.
+     *
+     * @var array
+     */
+    protected $policies = [
+        // 'App\Models\Model' => 'App\Policies\ModelPolicy',
+    ];
+
+    /* {{@snippet:end_variables}} */
+
+    /* {{@snippet:begin_methods}} */
+
+    /**
+     * Register any authentication / authorization services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        /* {{@snippet:begin_boot_method}} */
+
+        $this->registerPolicies();
+        
+        /* {{@snippet:end_boot_method}} */
+    }
+
+    /* {{@snippet:end_methods}} */
+
+}
+/* {{@snippet:end_class}} */
